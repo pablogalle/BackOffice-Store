469a2e52604f3093fd946bf5300231d18a3a31ec
Pablo Gallego <pablo.gallego.penas@gmail.com>
2023-05-16 18:43:16 +0200

    Mayor update: last: image selector

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CategoryListComponent } from './entities/category/category-list/category-list.component';
+import { ItemListComponent } from './entities/item/item-list/item-list.component';
+import { ItemFormComponent } from './entities/item/item-form/item-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'categories', component: CategoryListComponent },
+  { path: 'items', component: ItemListComponent },
+  { path: 'categories/:categoryId/items', component: ItemListComponent },
+  { path: 'items/:itemId', component: ItemFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/entities/category/category-list/category-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../model/category.model';
+import { CategoryService } from '../service/category.service';
+
+@Component({
+  selector: 'app-category-list',
+  templateUrl: './category-list.component.html',
+  styleUrls: ['./category-list.component.scss']
+})
+export class CategoryListComponent implements OnInit{
+
+  categories: Category[] = [];
+
+  constructor(private categoryService: CategoryService){}
+
+  ngOnInit(): void {
+    this.getCategories();
+    
+  }
+
+  getCategories() : void{
+    this.categoryService.getAllCategories().subscribe({
+      next: (categoriesRequest) => {this.categories = categoriesRequest;},
+      error: (err) => this.handleError(err)
+    })
+  }
+
+  handleError(err: any): void {
+    console.log(err)
+  }
+}
